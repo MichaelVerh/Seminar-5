5d10cb7018643007361b5f43556cf58fb47acded
MichaelVerh <107642304+MichaelVerh@users.noreply.github.com>
2022-07-27 23:08:08 +0300

    Add files via upload

---
+++ Program-HW-T2.cs
@@ -0,0 +1,35 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на четных позициях.
+int[] array=new int[8];
+    FillArray(array, 0, 10);
+    PrintArray(array);
+
+    int sum=0;
+    for(int i=0; i<array.Length; i++)
+    {
+        if (i%2==1) sum+=array[i];
+    }
+    Console.WriteLine($"Сумма элементов, стоящих на нечетных местах, равна {sum}");
+
+
+
+
+void FillArray(int[] array, int startNumber=0, int finishNumber=1)
+{
+    finishNumber++;
+    Random random=new Random();
+    for (int i=0; i<array.Length; i++)
+    {
+        array[i]=random.Next(startNumber, finishNumber);
+    }
+}
+
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine("Вывод массива");
+    for (int i=0; i<array.Length; i++)
+    {
+        Console.Write(array[i]+"\t");
+    }
+    Console.WriteLine();
+}
---
+++ Program-HW-T3.cs
@@ -0,0 +1,25 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементом массива.
+Random random=new Random();
+double[] array =new double[10];
+
+for (int i=0; i<array.Length; i++)
+{
+    array[i]=Math.Round(random.NextDouble()*10-5, 2);
+}
+Console.WriteLine();
+for (int i=0; i<array.Length; i++)
+{
+    Console.Write(array[i]+"\t");
+}
+Console.WriteLine();
+
+double max=array[0];
+double min=array[0];
+for (int i=1; i<array.Length; i++)
+{
+    if (array[i]>max) max=array[i];
+    else if (array[i]<min) min=array[i];
+}
+Console.WriteLine($"Максимальное значение равно {max}");
+Console.WriteLine($"Минимальное значение равно {min}");
+Console.WriteLine($"Разность между максимальным и минимальным элементом массива равна {max-min}");
